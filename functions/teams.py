8ff40bd28d4f93b6b52f3f948ab1d33df06b1a95
Vadim Arsenev <vvarsenev@gmail.com>
2023-09-02 15:14:27 +0300

    fix

--- functions/teams.py
+++ functions/teams.py
@@ -8,36 +8,36 @@ from simple_settings import settings
 
 
 __author__ = 'Vadim Arsenev'
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 __data__ = '01.09.2023'
 
 
 def calculate(players, teams):
     df = pd.DataFrame()
     for item in range(len(teams)):
-        couch = teams[item][-1]
-        couch_points = players.loc[(players['Abbr'] == couch) & (players['st'] == 0) \
+        coach = teams[item][-1]
+        coach_points = players.loc[(players['Abbr'] == coach) & (players['st'] == 0) \
                                    & (players['min'] > 0), 'points'].sum()
-        couch_game = len(players.loc[(players['Abbr'] == couch) & (players['st'] > 0)])
-        if couch_game == 0:
-            couch_game = '+ couch'
+        coach_game = len(players.loc[(players['Abbr'] == coach) & (players['st'] > 0)])
+        if coach_game == 0:
+            coach_game = '+ coach'
         else:
-            couch_game = ''
+            coach_game = ''
 
         players_no_games = len(players.loc[players['id'].isin(teams[item][0:-2]) \
                     & (players['st'] == 0) & (players['min'] == 0) & (players['Pos'] != 'GK')])
         if players_no_games == 0:
             player_points = players.loc[players['id'].isin(teams[item][0:-2]), 'points'].sum() \
-                + couch_points
+                + coach_points
         else:
             player_points = players.loc[players['id'].isin(teams[item][0:-1]), 'points'].sum() \
-                + couch_points
+                + coach_points
         
         count_players_in = len(players.loc[players['id'].isin(teams[item][0:-2]) \
                     & (players['min'] > 0)])
 
         df_temp = pd.DataFrame([[teams[item][0], int(player_points), 11-int(count_players_in), \
-                    couch_game]], columns=settings.COLUMNS)
+                    coach_game]], columns=settings.COLUMNS)
         df = pd.concat([df, df_temp], ignore_index=True)
     df = df.sort_values(by=['points'], ascending=False)
     
